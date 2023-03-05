439b678b30fd3d37f7e81bd54ced32ec85c63436
EDyugaeva <ed2408@yandex.ru>
2023-03-05 19:54:01 +0500

    optimize logger

--- src/main/java/pro/sky/animal_shelter_telegram_bot/service/impl/PetServiceImpl.java
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/service/impl/PetServiceImpl.java
@@ -1,7 +1,6 @@
 package pro.sky.animal_shelter_telegram_bot.service.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pro.sky.animal_shelter_telegram_bot.model.pets.Pet;
 import pro.sky.animal_shelter_telegram_bot.repository.PetRepository;
@@ -13,9 +12,8 @@ import java.util.Collection;
  * Service for working with repository DogRepository
  */
 @Service
+@Slf4j
 public class PetServiceImpl implements PetService {
-
-    Logger logger = LoggerFactory.getLogger(PetServiceImpl.class);
 
     private final PetRepository petRepository;
 
@@ -26,40 +24,40 @@ public class PetServiceImpl implements PetService {
     @Override
     public Pet addPet(Pet pet) {
         Pet addingPet = petRepository.save(pet);
-        logger.info("Pet with id {} is saved", addingPet.getId());
+        log.info("Pet with id {} is saved", addingPet.getId());
         return addingPet;
     }
 
     public Pet deletePet(Long id) {
         if (petRepository.findById(id).isEmpty()){
-            logger.info("Pet with id {} is not found", id);
+            log.info("Pet with id {} is not found", id);
             return null;
         }
         Pet deletePet = petRepository.findById(id).get();
         petRepository.deleteById(id);
-        logger.info("Pet with id {} is deleted", id);
+        log.info("Pet with id {} is deleted", id);
         return deletePet;
     }
 
     @Override
     public Pet findPet(Long id) {
         if (petRepository.findById(id).isEmpty()){
-            logger.info("Pet with id {} is not found", id);
+            log.info("Pet with id {} is not found", id);
             return null;
         }
         Pet findingPet = petRepository.findById(id).get();
-        logger.info("Pet with id {} is found", id);
+        log.info("Pet with id {} is found", id);
         return findingPet;
     }
 
     @Override
     public Pet changePet(Pet pet) {
         if (petRepository.findById(pet.getId()).isEmpty()){
-            logger.info("Pet with id {} is not found", pet.getId());
+            log.info("Pet with id {} is not found", pet.getId());
             return null;
         }
         Pet changingPet = petRepository.save(pet);
-        logger.info("Pet with id {} is saved", pet);
+        log.info("Pet with id {} is saved", pet);
         return changingPet;
     }
 
@@ -70,7 +68,7 @@ public class PetServiceImpl implements PetService {
      */
     @Override
     public Collection<Pet> getAllPets() {
-        logger.info("Was invoked method for getAllPets");
+        log.info("Was invoked method for getAllPets");
         return petRepository.findAll();
     }
 
